31e90c5bde1cf435eb6d9cb994719fad28843471
Genie <genie0320@gmail.com>
2024-07-26 20:42:26 +0900

    add - homeCards

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
 import { normalizeClass } from 'vue';
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <HomeCards />
 </template>
---
+++ vue-crash-2024/src/components/Card.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    bg: {
+        type: String,
+        default:'bg-gray-100',
+    },
+})
+</script>
+
+<template>
+<!-- ` ` 안에 있는 것은 무엇이든 js로 인식한다. -->
+<div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+</div>    
+</template>
