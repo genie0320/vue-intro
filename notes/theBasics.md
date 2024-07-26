3c7c3cf8fa4e6a261c485c292aaf6c2d053cc9aa
Genie <genie0320@gmail.com>
2024-07-26 19:00:55 +0900

    class - day03

---
+++ notes/theBasics.md
@@ -0,0 +1,20 @@
+# Issues
+[ ] @ 과 : 언제 사용하는 것이며 차이는 무엇인가?
+```
+v-bind:href == :href
+v-on:click == @click
+```
+
+# Lifecycle Methods of vue.js
+
+Lifecycle Methods
+
+-`onBeforeMount`: Called before mounting begins.  
+-`onMounted`: Called when the component is mounted.  
+-`onBeforeUpdate`: Called when reactive data changes and before re-rendering.  
+-`onUpdated`: Called after re-rendering.  
+-`onBeforeUnmount`: Called before the Vue instance is destroyed.  
+-`onUnmounted`: Called after the instance is destroyed.  
+-`onActivated`: Called when a kept-alive component is activated.  
+-`onDeactivated`: Called when a kept-alive component is deactivated.  
+-`onErrorCaptured`: Called when an error is captured from a child component.  
--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -1,45 +1,11 @@
-<!-- ■■■ A long version ■■■ -->
-<!-- 
-<script>
-import { ref } from 'vue'
-
-// composition API
-export default {
-
-  setup() {
-    const name = ref('Genie Park');
-    const status = ref('active');
-    const tasks = ref(['task 01', 'task 02', 'task 03']);
-
-    const toggleStatus = () => {
-      if (status.value === 'active') {
-        status.value = 'pending';
-      } else if (status.value === 'pending') {
-        status.value = 'inactive';
-      } else {
-        status.value = 'active';
-      }
-    }
-
-    return {
-      name,
-      status,
-      tasks,
-      toggleStatus
-    }
-  }  
-}
-</script> 
--->
-
-<!-- ■■■ A shorter version ■■■-->
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 // for composition API
 const name = ref('Genie Park');
 const status = ref('active');
 const tasks = ref(['task 01', 'task 02', 'task 03']);
+const newTask = ref('')
 
 const toggleStatus = () => {
   if (status.value === 'active') {
@@ -50,6 +16,27 @@ const toggleStatus = () => {
     status.value = 'active';
   }
 }
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+}
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1)
+}
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.log('Error fetching tasks')
+  }
+})
 </script>
 
 <template>
@@ -58,16 +45,23 @@ const toggleStatus = () => {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form action="" @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="createTask" name="new-Task" v-model='newTask'>
+  </form>
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
 
-  <!-- <a v-bind:href="link">Click for Google</a> -->
   <!-- <a :href="link">Click for Google</a>  -->
 
   <hr>
-  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
   <button @click="toggleStatus">Change Status</button>
 
 </template>
