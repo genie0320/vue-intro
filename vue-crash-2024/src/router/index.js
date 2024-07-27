fedbdc61d50468e2efea05c0e279ec9804843a82
Genie <genie0320@gmail.com>
2024-07-27 21:53:04 +0900

    add - job detail, page not found

--- vue-crash-2024/src/router/index.js
+++ vue-crash-2024/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
 import JobsView from '@/views/JobsView.vue'
+import NotFound from '@/views/NotFound.vue'
+import JobDetailView from '@/views/JobDetailView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +17,16 @@ const router = createRouter({
             path: '/jobs',
             name: 'jobs',
             component : JobsView
+        },
+        {
+            path: '/jobs/:id',
+            name: 'job-detail',
+            component : JobDetailView
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: 'not-found',
+            component : NotFound
         },
     ]
 })
