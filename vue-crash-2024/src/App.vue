01d1aceac6e1823bf60db1be774a3f87e079275c
Genie <genie0320@gmail.com>
2024-07-27 16:03:20 +0900

    working - router

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -1,14 +1,10 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
-import Hero from '@/components/Hero.vue';
-import HomeCards from '@/components/HomeCards.vue';
-import JobListings from '@/components/JobListings.vue';
+import { RouterView } from 'vue-router';
 import { normalizeClass } from 'vue';
 </script>
 
 <template>
   <Navbar />
-  <Hero />
-  <HomeCards />
-  <JobListings />
+  <RouterView />
 </template>
